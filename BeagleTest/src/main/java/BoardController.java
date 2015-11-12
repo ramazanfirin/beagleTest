4de48ebc5bbf6c7877c675a3c450741555ad3887
Ramazan FIRIN <ramazan_firin@hotmail.com>
2015-11-12 08:32:58 +0200

    initial

---
+++ BeagleTest/src/main/java/BoardController.java
@@ -0,0 +1,36 @@
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+@ApplicationScoped
+public class BoardController {
+	private String sicaklik="34";
+	private String nem="53";
+	private String odaBasinci="65";
+	private String filtreBasinci="115";
+	
+	public String getSicaklik() {
+		return sicaklik;
+	}
+	public void setSicaklik(String sicaklik) {
+		this.sicaklik = sicaklik;
+	}
+	public String getNem() {
+		return nem;
+	}
+	public void setNem(String nem) {
+		this.nem = nem;
+	}
+	public String getOdaBasinci() {
+		return odaBasinci;
+	}
+	public void setOdaBasinci(String odaBasinci) {
+		this.odaBasinci = odaBasinci;
+	}
+	public String getFiltreBasinci() {
+		return filtreBasinci;
+	}
+	public void setFiltreBasinci(String filtreBasinci) {
+		this.filtreBasinci = filtreBasinci;
+	}
+}
