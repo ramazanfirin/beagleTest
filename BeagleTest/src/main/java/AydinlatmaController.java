d83b621ef28df9e720d1c466653fbe7a107fe177
Ramazan FIRIN <ramazan_firin@hotmail.com>
2015-12-02 08:36:28 +0200

    commandButton

--- BeagleTest/src/main/java/AydinlatmaController.java
+++ BeagleTest/src/main/java/AydinlatmaController.java
@@ -11,4 +11,40 @@ Lamba lamba1= new Lamba();
 Lamba lamba2= new Lamba();	
 Lamba lamba3= new Lamba();	
 Lamba lamba4= new Lamba();	
+
+public void changeLamba1Status(){
+	lamba1.setStatus(!lamba1.getStatus());
+}
+
+public Lamba getLamba1() {
+	return lamba1;
+}
+
+public void setLamba1(Lamba lamba1) {
+	this.lamba1 = lamba1;
+}
+
+public Lamba getLamba2() {
+	return lamba2;
+}
+
+public void setLamba2(Lamba lamba2) {
+	this.lamba2 = lamba2;
+}
+
+public Lamba getLamba3() {
+	return lamba3;
+}
+
+public void setLamba3(Lamba lamba3) {
+	this.lamba3 = lamba3;
+}
+
+public Lamba getLamba4() {
+	return lamba4;
+}
+
+public void setLamba4(Lamba lamba4) {
+	this.lamba4 = lamba4;
+}
 }
